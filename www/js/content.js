1ff1e3326637a6e04258fd60afddb49522601e4e
lrojas12 <r.luisaf@gmail.com>
2017-02-05 05:00:46 -0500

    Edited notification HTML and CSS, as well as the content.js file to generate it

--- www/js/content.js
+++ www/js/content.js
@@ -1,68 +1,58 @@
-main();
+var username = "Luisa"; //TODO: get name from the database 
+var message = "<b>Bot</b>: Hello, " + username + ". How are you doing today?";
 
-function main(){
-    console.log("Hello world");
+createNotification(message);
 
-    var name = "Luisa"; //TODO: get name from the database
-    var message = "Hey " + name + ", hru?";
-    createNotification(message);
-    initBtn();
-    initInput();
-}
+$("#send-btn").click(function() {
+    //console.log("Go Button Clicked!");
 
-//---------HELPER FUNCTIONS---------
-function createNotification(notification_message){
-    //Build the notification
-    var $button = $("<button id='send-btn' class='btn btn-default' type='button'>").html("Send");
-    var $span = $("<span class='input-group-btn'>").append($button);
-    var $input = $("<input id='sentiment_input' type='text' class='form-control' placeholder='I feel...'>");
-    var $input_group = $("<div class='input-group'>").append($input, $span);
-    var $col = $("<div class='col-sm-12'>").append($input_group);
-    var $row = $("<div class='row'>").append($col);
-    var $notif_msg = $("<div id='notification_msg'>").html(notification_message);
-    var $notification = $("<div class='well well-sm' id='notification'>").append($notif_msg);
+    //Obtain the input from the user
+    var $input = $("#sentiment_input");
+    var input = $input.val();
+    console.log("[client] " + input);
 
-    //Append the notification to the body
-    $notification.append($row);
-    $("body").append($notification);
-}
-
-function initBtn(){
-    
-    $("#send-btn").click(function(){
-	//console.log("Go Button Clicked!");
-
-	//Obtain the input from the user
-	var $input = $("#sentiment_input");
-	var input = $input.val();
-	console.log("[client] " + input);
-	
-	//Send post request to the server
-	$.post('http://127.0.0.1:3000/processSentiment', {input:input})
+    //Send post request to the server
+    $.post('http://127.0.0.1:3000/processSentiment', {input:input})
         .done(function(data) {
-		
-    		var reply = data.message;
-    		
+        
+            var reply = data.message;
+            
             //TODO: Use UI to display reply to user
-		
+        
             console.log(reply);
         })
         .fail(function(xhr, textStatus, error) {
-    		console.log(xhr.statusText);
-    		console.log(textStatus);
-    		console.log(error);
+            console.log(xhr.statusText);
+            console.log(textStatus);
+            console.log(error);
     });
 
-	//Clear the input field
-	$input.val(''); 
-    });
-}
+    //Clear the input field
+    $input.val(''); 
+});
 
-function initInput(){
-    $("#sentiment_input").keyup(function(event){
-        
-    	if(event.keyCode == 13){
-                $("#send-btn").click();
-    	}
-    });
+$("#sentiment_input").keyup(function(event){
+    
+    if(event.keyCode == 13){
+            $("#send-btn").click();
+    }
+});
+
+
+function createNotification(notification_message) {
+    
+    var $button = $("<button id='send-btn' class='btn btn-default' type='button'>").html("Send");
+    var $span = $("<span class='input-group-btn'>").append($button);
+    var $input = $("<input id='sentiment-input' type='text' class='form-control' placeholder='I feel ...'>");
+    var $input_group = $("<div class='input-group'>").append($input, $span);
+    var $notif_msg = $("<div id='notification-msg'>").html(notification_message);
+    var $chat_holder = $("<div id='chat-holder'>").append($notif_msg, $input_group);
+    
+    var $header_text = $("<p>").html("New message!");
+    var $notif_header = $("<div id='notification-header'>").append($header_text);
+    
+    var $notification = $("<div class='well well-sm' id='notification'>").append($notif_header, $chat_holder);
+
+    //Append the notification to the body
+    $("body").append($notification);
 }
