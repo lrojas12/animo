4a697b75853a12a7be42288d5126f0fc75cf925f
alex <uwalex@gmail.com>
2017-02-04 21:35:40 -0500

    Hello world base app

---
+++ www/js/content.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+
+chrome.runtime.onMessage.addListener(
+    function(req, sender, sendResponse){
+	if(req.message == "clicked_browser_action"){
+	    //grab the first url
+	    var firstHref = $("a[href^='http']").eq(0).attr("href");
+	    console.log(firstHref);
+
+	    chrome.runtime.sendMessage({"message": "open_new_tab", "url":firstHref});
+	}
+    }
+);
