cbacfff2153cbb43371f3c0ec356bd07066dd283
lrojas12 <r.luisaf@gmail.com>
2017-02-07 00:26:36 -0500

    Now, the main popup also keeps track of and saves usernames.

--- www/js/popup.js
+++ www/js/popup.js
@@ -11,90 +11,94 @@ chrome.storage.local.get(['username', 'freq'], function(items) {
       username = items.username;
       freq = items.freq;
       console.log('loaded up variables');
+      
+      app();
 });
 
-if (!username) {
-    $("#greeting").html("Hello, there!");
-    $("#username_input").attr("placeholder", "New username here!");
-    user_exists = false;
-} else {
-    $("#username_input").val(username);
-    $('#dropdown-btn').html(freq);
-    $("#greeting").html("Hello, " + username + "!");
-    $("#username_input").attr("placeholder", "New username?");
-    user_exists = true;
-}
-
-$("#username_input").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#save-btn").click();
+function app() {
+    if (!username) {
+        $("#greeting").html("Hello, there!");
+        $("#username_input").attr("placeholder", "New username here!");
+        user_exists = false;
+    } else {
+        $("#username_input").val(username);
+        $('#dropdown-btn').html(freq);
+        $("#greeting").html("Hello, " + username + "!");
+        $("#username_input").attr("placeholder", "New username?");
+        user_exists = true;
     }
-});
 
-$("#unordered-list li a").click(function() {
-    $("#dropdown-btn").html($(this).html());
-});
-
-$("#analyze-btn").click(function() {
-    
-    var $newdiv = $("<div class='alert'>").html("<b>Coming up soon</b>: You will be able to see a detailed analysis of your moods and experiences depending on the spawn frequency you have chosen. This would be done using the database where all user data is store, as well as Sentiment Analysis NLP.");
-    $("#analyze-div").append($newdiv);
-    
-    setTimeout(function(){$('.alert').remove()}, 20000);
-    
-});
-
-$('#save-btn').click(function() {
-    
-    $("#username_error").html("");
-    $("#freq_error").html("");
-    $("#submission_msg").html("");
-    
-    var username = $("#username_input").val();
-    console.log("Username entered: " + username);
-    var freq = $('#dropdown-btn').html();
-    console.log("Freq chosen: " + freq);    
-    
-    //TODO: check if there is a notification spawn frequency saved in local storage.
-    //otherwise, show error and remind user to pick an option.
-
-    if (!user_exists) { //new user
-        if (!username) {
-            $("#username_error").html("You're a new user; please pick a username.");
-            return;
+    $("#username_input").keyup(function(event){
+        if(event.keyCode == 13){
+            $("#save-btn").click();
         }
-        
-        if (username.length > 9 || username.length < 3) {
-            $("#username_error").html("Your username should be 3-9 characters.");
-            return;
-        }
-        
-        if (freq == "Please select one") {
-            $("#freq_error").html("You're a new user; please select one.");
-            return;
-        }
-    }
-    
-    //Save stuff locally!
-    //Check if local storage is supported
-    /*
-    if (typeof(Storage) == "undefined") {
-	       console.log("localStorage not supported");
-	          return;
-    }
-    */
-    
-    $("#submission_msg").html("Your changes have been saved successfully.");
-    $("#greeting").html("Hello, " + username + "!");
-    $("#username_input").attr("placeholder", "New username?");
-    
-    //Save the username
-    //localStorage.setItem("qhacks_username", username);
-    //localStorage.setItem("qhacks_freq", freq);
-    chrome.storage.local.set({'username': username, 'freq':freq}, function() {
-        console.log('saved username and freq');
     });
-});
+
+    $("#unordered-list li a").click(function() {
+        $("#dropdown-btn").html($(this).html());
+    });
+
+    $("#analyze-btn").click(function() {
+        
+        var $newdiv = $("<div class='alert'>").html("<b>Coming up soon</b>: You will be able to see a detailed analysis of your moods and experiences depending on the spawn frequency you have chosen. This would be done using the database where all user data is store, as well as Sentiment Analysis NLP.");
+        $("#analyze-div").append($newdiv);
+        
+        setTimeout(function(){$('.alert').remove()}, 20000);
+        
+    });
+
+    $('#save-btn').click(function() {
+        
+        $("#username_error").html("");
+        $("#freq_error").html("");
+        $("#submission_msg").html("");
+        
+        var username = $("#username_input").val();
+        console.log("Username entered: " + username);
+        var freq = $('#dropdown-btn').html();
+        console.log("Freq chosen: " + freq);    
+        
+        //TODO: check if there is a notification spawn frequency saved in local storage.
+        //otherwise, show error and remind user to pick an option.
+
+        if (!user_exists) { //new user
+            if (!username) {
+                $("#username_error").html("You're a new user; please pick a username.");
+                return;
+            }
+            
+            if (username.length > 9 || username.length < 3) {
+                $("#username_error").html("Your username should be 3-9 characters.");
+                return;
+            }
+            
+            if (freq == "Please select one") {
+                $("#freq_error").html("You're a new user; please select one.");
+                return;
+            }
+        }
+        
+        //Save stuff locally!
+        //Check if local storage is supported
+        /*
+        if (typeof(Storage) == "undefined") {
+    	       console.log("localStorage not supported");
+    	          return;
+        }
+        */
+        
+        $("#submission_msg").html("Your changes have been saved successfully.");
+        $("#greeting").html("Hello, " + username + "!");
+        $("#username_input").attr("placeholder", "New username?");
+        
+        //Save the username
+        //localStorage.setItem("qhacks_username", username);
+        //localStorage.setItem("qhacks_freq", freq);
+        chrome.storage.local.set({'username': username, 'freq':freq}, function() {
+            console.log('saved username and freq');
+        });
+    });
+}
 
 //For debuggin purposes to simulate first time users
 function resetLocalStorage(){
